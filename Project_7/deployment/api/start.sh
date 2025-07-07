c3d2b0d97eca83b4d07d585ce58b864760dec91e
Grégoire MUREAU <gregoire.mureau+pro@etik.com>
2025-07-07 22:02:02 +0200

    fix for start.sh for modest CPUs

--- Project_7/deployment/api/start.sh
+++ Project_7/deployment/api/start.sh
@@ -4,4 +4,4 @@
 streamlit run app.py --server.port 8501 --server.address 0.0.0.0 &
 
 # Lancer Uvicorn en premier plan (Docker "suit" ce processus)
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000 --loop asyncio
